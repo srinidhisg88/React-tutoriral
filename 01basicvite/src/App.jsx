943d5210652fdf842a3455616c91bb6de475aea3
srinidhisg88 <srinidhisg1@gmail.com>
2024-11-03 01:25:22 +0530

    react and vite

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,19 @@
+import Youtube from "./youtube.jsx"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>
+      classic react p 
+    </h1>
+    <h1>classic react app</h1>
+    <Youtube/>
+    </>
+  
+  )
+}
+
+export default App
