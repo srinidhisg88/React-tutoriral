bff33d65bffa8ca5cc72de027a6d19e55eb0c594
srinidhisg88 <srinidhisg1@gmail.com>
2024-11-03 19:33:44 +0530

    new

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -3,11 +3,11 @@ import Youtube from "./youtube.jsx"
 
 function App() {
   
-
+  const username='srinidhi'
   return (
     <>
     <h1>
-      classic react p 
+      classic react {2+2} 
     </h1>
     <h1>classic react app</h1>
     <Youtube/>
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,10 +1,23 @@
 
 import { createRoot } from 'react-dom/client'
 
-import App from './App.jsx'
+ import App from './App.jsx'
+
+
+// const reactElement={
+//     type:'a',
+//     props:{
+//         href:'https://google.com',
+//         target:'_blank'
+//     },
+//     children:'Click me to visit google'
+// }
+
+
+
 
 createRoot(document.getElementById('root')).render(
 
-    <App />
+   <App/>
 
 )
